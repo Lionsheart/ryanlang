f3d4e3b7814a95a82901ea3d88e139c18bb5e32f
Lionsheart <Lionsheart81@gmail.com>
2020-12-17 14:46:23 -0600

    portfolio container

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,88 +1,91 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="row gallery">
-        <div class="col-12 text-center">
-            <h2>Portfolio</h2>
-        </div>
-        <div class="col-md-12">
-            <ul class="thumbnails">
+    <div class="container">
+      <div class="row gallery">
+          <div class="col-12 text-center">
+              <h2>Portfolio</h2>
+          </div>
+          <div class="col-md-12">
+              <ul class="thumbnails">
 
-                <li>
-                    <a href="/images/slides/responsive_homepage.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Responsive e-commerce homepage">
-                        <img src="/images/thumbnails/responsive_homepage.jpg"/>
-                        <h5>Responsive storefront</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/product_showcase.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Custom site showcasing a product line.">
-                        <img src="/images/thumbnails/product_showcase.jpg"/>
-                        <h5>Product Showcase</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/wireless_store.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Layout and design of wireless store">
-                        <img src="/images/thumbnails/wireless_store.jpg"/>
-                        <h5>Wireless Store</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/roofing_site.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Hornback Roofing">
-                        <img src="/images/thumbnails/roofing_site_tn.jpg"/>
-                        <h5>Hornback Roofing</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/1280_home.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="E-commerce Layout">
-                        <img src="/images/thumbnails/1280_home_tn.jpg"/>
-                        <h5>E-commerce home layout</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/dashboard2.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Dashboard Layout">
-                        <img src="/images/thumbnails/dashboard2_tn.jpg"/>
-                        <h5>Dashboard 2</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/dashboard.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Account dashboard redesign">
-                        <img src="/images/thumbnails/dashboard.jpg"/>
-                        <h5>User Dashboard</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/application.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Web based customer application">
-                        <img src="/images/thumbnails/online_application.jpg"/>
-                        <h5>Application</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/golla_promotion.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Product Line Showcase">
-                        <img src="/images/thumbnails/golla_promotion_tn.jpg"/>
-                        <h5>Product Line Showcase</h5>
-                    </a>
-                </li>
-                <li>
-                    <a href="/images/slides/resp_email.jpg" class="thumbnail" data-lightbox="portfolio"
-                       data-title="Responsive E-mail">
-                        <img src="/images/thumbnails/resp_email_tn.jpg"/>
-                        <h5>Responsive E-mail</h5>
-                    </a>
-                </li>
+                  <li>
+                      <a href="/images/slides/responsive_homepage.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Responsive e-commerce homepage">
+                          <img src="/images/thumbnails/responsive_homepage.jpg"/>
+                          <h5>Responsive storefront</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/product_showcase.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Custom site showcasing a product line.">
+                          <img src="/images/thumbnails/product_showcase.jpg"/>
+                          <h5>Product Showcase</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/wireless_store.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Layout and design of wireless store">
+                          <img src="/images/thumbnails/wireless_store.jpg"/>
+                          <h5>Wireless Store</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/roofing_site.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Hornback Roofing">
+                          <img src="/images/thumbnails/roofing_site_tn.jpg"/>
+                          <h5>Hornback Roofing</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/1280_home.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="E-commerce Layout">
+                          <img src="/images/thumbnails/1280_home_tn.jpg"/>
+                          <h5>E-commerce home layout</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/dashboard2.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Dashboard Layout">
+                          <img src="/images/thumbnails/dashboard2_tn.jpg"/>
+                          <h5>Dashboard 2</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/dashboard.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Account dashboard redesign">
+                          <img src="/images/thumbnails/dashboard.jpg"/>
+                          <h5>User Dashboard</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/application.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Web based customer application">
+                          <img src="/images/thumbnails/online_application.jpg"/>
+                          <h5>Application</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/golla_promotion.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Product Line Showcase">
+                          <img src="/images/thumbnails/golla_promotion_tn.jpg"/>
+                          <h5>Product Line Showcase</h5>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/images/slides/resp_email.jpg" class="thumbnail" data-lightbox="portfolio"
+                         data-title="Responsive E-mail">
+                          <img src="/images/thumbnails/resp_email_tn.jpg"/>
+                          <h5>Responsive E-mail</h5>
+                      </a>
+                  </li>
 
 
-            </ul>
-        </div>
+              </ul>
+          </div>
+      </div>
     </div>
+
 @endsection
 
 @push('scripts')
